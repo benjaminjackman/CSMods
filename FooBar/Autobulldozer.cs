07efda30d430a4a944a02190fdaa562e682cb224
Ben Jackman <ben@jackman.biz>
2015-03-13 08:56:11 -0500

    Initial Commit, Nothing works well
    Signed-off-by: Ben Jackman <ben@jackman.biz>

---
+++ FooBar/Autobulldozer.cs
@@ -0,0 +1,110 @@
+﻿using ColossalFramework;
+using ICities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+//namespace FooBar
+//{
+//	public class AutobulldozeMod : IUserMod
+//	{
+//		public static GameObject modObject;
+//
+//		public string Name { 
+//			get { return "Automatic Bulldoze"; } 
+//		} 
+//		public string Description { 
+//			get { return "Automatically destroys abandoned and burned buildings"; } 
+//		}
+//
+//	}
+//
+//	public class ThreadingTestMod : ThreadingExtensionBase
+//	{
+//		public static AudioGroup nullAudioGroup;
+//
+//		public override void OnCreated(IThreading threading)
+//		{
+//			UnityEngine.Debug.LogWarning("Autobulldoze initialized.");
+//			nullAudioGroup = new AudioGroup(0, new SavedFloat("NOTEXISTINGELEMENT", Settings.gameSettingsFile, 0, false));
+//		}
+//
+//		public override void OnUpdate(float realTimeDelta, float simulationTimeDelta)
+//		{
+//
+//
+//
+//		}
+//
+//		private int GetBuildingRefundAmount(ushort building)
+//		{
+//			BuildingManager instance = Singleton<BuildingManager>.instance;
+//			if (Singleton<SimulationManager>.instance.IsRecentBuildIndex(instance.m_buildings.m_buffer[(int) building].m_buildIndex))
+//				return instance.m_buildings.m_buffer[(int) building].Info.m_buildingAI.GetRefundAmount(building, ref instance.m_buildings.m_buffer[(int) building]);
+//			else
+//				return 0;
+//		}
+//
+//
+//		public static void DispatchAutobulldozeEffect(BuildingInfo info, Vector3 pos, float angle, int length)
+//		{
+//			EffectInfo effect = Singleton<BuildingManager>.instance.m_properties.m_bulldozeEffect;
+//			if (effect == null) return;
+//			InstanceID instance = new InstanceID();
+//			EffectInfo.SpawnArea spawnArea = new EffectInfo.SpawnArea(Matrix4x4.TRS(Building.CalculateMeshPosition(info, pos, angle, length), Building.CalculateMeshRotation(angle), Vector3.one), info.m_lodMeshData);
+//			Singleton<EffectManager>.instance.DispatchEffect(effect, instance, spawnArea, Vector3.zero, 0.0f, 1f, nullAudioGroup);
+//		}
+//
+//
+//		private void DeleteBuildingImpl(ushort building)
+//		{
+//			if (Singleton<BuildingManager>.instance.m_buildings.m_buffer[(int) building].m_flags != Building.Flags.None)
+//			{
+//				BuildingManager instance = Singleton<BuildingManager>.instance;
+//				BuildingInfo info = instance.m_buildings.m_buffer[(int) building].Info;
+//				if (info.m_buildingAI.CheckBulldozing(building, ref instance.m_buildings.m_buffer[(int) building]) == ToolBase.ToolErrors.None)
+//				{
+//					if (info.m_placementStyle == ItemClass.Placement.Automatic)
+//					{
+//					}
+//					int buildingRefundAmount = this.GetBuildingRefundAmount(building);
+//					if (buildingRefundAmount != 0)
+//						Singleton<EconomyManager>.instance.AddResource(EconomyManager.Resource.RefundAmount, buildingRefundAmount, info.m_class);
+//					Vector3 pos = instance.m_buildings.m_buffer[(int) building].m_position;
+//					float angle = instance.m_buildings.m_buffer[(int) building].m_angle;
+//					int length = instance.m_buildings.m_buffer[(int) building].Length;
+//					instance.ReleaseBuilding(building);
+//					if (info.m_class.m_service > ItemClass.Service.Office)
+//						Singleton<CoverageManager>.instance.CoverageUpdated(info.m_class.m_service, info.m_class.m_subService, info.m_class.m_level);
+//					DispatchAutobulldozeEffect(info, pos, angle, length);
+//				}
+//			}
+//		}
+//
+//
+//
+// 		public override void OnAfterSimulationTick()
+//		{
+//			SimulationManager simManager = Singleton<SimulationManager>.instance;
+//			BuildingManager buildManager = Singleton<BuildingManager>.instance;
+//
+//			for (ushort i = (ushort)(simManager.m_currentTickIndex%1000); i < buildManager.m_buildings.m_buffer.Length; i+=1000)
+//			{
+//				Building build = buildManager.m_buildings.m_buffer[i];
+//
+//				if ((build.m_flags & (Building.Flags.BurnedDown | Building.Flags.Abandoned)) != Building.Flags.None)
+//				{
+//					UnityEngine.Debug.LogWarning("Autobulldozed #" + i);
+//					DeleteBuildingImpl(i);
+//					return;
+//				}
+//
+//			}
+//		}
+//
+//
+//	}
+//}
---
+++ FooBar/MyClass.cs
@@ -0,0 +1,204 @@
+﻿using ColossalFramework.UI;
+using ColossalFramework;
+using ICities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+
+namespace FooBar
+{
+
+	public class OneBigPipeMod : IUserMod
+	{
+		public static GameObject modObject;
+
+		public string Name { 
+			get { 
+				Console.WriteLine ("HELLO WORLD0");
+				return "OneBigPipeMod"; 
+			} 
+		} 
+		public string Description { 
+			get { 
+				Console.WriteLine ("HELLO WORLD0");
+				return "Makes the water radius of pipes really large"; 
+			} 
+		}
+
+	}
+
+	public class LoadingExtension : LoadingExtensionBase
+	{
+		public override void OnLevelLoaded(LoadMode mode)
+		{
+			Console.WriteLine ("HELLO WORLD2");
+
+			DebugOutputPanel.AddMessage (ColossalFramework.Plugins.PluginManager.MessageType.Message, "Message OLL");
+
+			// Get the UIView object. This seems to be the top-level object for most
+			// of the UI.
+			var uiView = UIView.GetAView();
+
+			// Add a new button to the view.
+			var button = (UIButton)uiView.AddUIComponent(typeof(UIButton));
+
+			// Set the text to show on the button.
+			button.text = "Click Me!";
+
+			// Set the button dimensions.
+			button.width = 100;
+			button.height = 30;
+
+			// Style the button to look like a menu button.
+			button.normalBgSprite = "ButtonMenu";
+			button.disabledBgSprite = "ButtonMenuDisabled";
+			button.hoveredBgSprite = "ButtonMenuHovered";
+			button.focusedBgSprite = "ButtonMenuFocused";
+			button.pressedBgSprite = "ButtonMenuPressed";
+			button.textColor = new Color32(255, 255, 255, 255);
+			button.disabledTextColor = new Color32(7, 7, 7, 255);
+			button.hoveredTextColor = new Color32(7, 132, 255, 255);
+			button.focusedTextColor = new Color32(255, 255, 255, 255);
+			button.pressedTextColor = new Color32(30, 30, 44, 255);
+
+			// Enable button sounds.
+			button.playAudioEvents = true;
+
+			// Place the button.
+			button.transformPosition = new Vector3(-1.65f, 0.97f);
+
+			// Respond to button click.
+			button.eventClick += ButtonClick;
+		}
+
+		private void ButtonClick(UIComponent component, UIMouseEventParameter eventParam)
+		{
+			Console.WriteLine ("BUC1");
+			// Do something
+			Type type = typeof(WaterPipeAI);
+			var builtIn = type.GetMethod ("GetEffectRadius");
+			Console.WriteLine ("BUC2");
+
+			var replacement = this.GetType ().GetMethod ("GetEffectRadius");
+
+			Console.WriteLine ("BUC3");
+
+			ReplaceMethod (replacement,builtIn);
+			Console.WriteLine ("BUC4");
+		}
+
+		public void GetEffectRadius (out float radius, out bool capped, out Color color)
+		{
+			Console.WriteLine ("Get Effect Radius!!");
+			radius = 900;
+			capped = false;
+			color = Singleton<InfoManager>.instance.m_properties.m_modeProperties [2].m_activeColor;
+		}
+
+		public static IntPtr GetMethodAddress(MethodBase method)
+		{
+			if ((method is DynamicMethod))
+			{
+				Console.WriteLine ("Dynamic Method");
+				unsafe
+				{
+					byte* ptr = (byte*)GetDynamicMethodRuntimeHandle(method).ToPointer();
+					if (IntPtr.Size == 8)
+					{
+						ulong* address = (ulong*)ptr;
+						address += 6;
+						return new IntPtr(address);
+					}
+					else 
+					{
+						uint* address = (uint*)ptr;
+						address += 6;
+						return new IntPtr(address);
+					}
+				}
+			}
+
+			RuntimeHelpers.PrepareMethod(method.MethodHandle);
+
+			unsafe
+			{
+				Console.WriteLine ("Post Dynamic Method");
+				// Some dwords in the met
+				int skip = 10;
+
+				// Read the method index.
+				UInt64* location = (UInt64*)(method.MethodHandle.Value.ToPointer());
+				int index = (int)(((*location) >> 32) & 0xFF);
+
+				if (IntPtr.Size == 8)
+				{
+					// Get the method table
+					ulong* classStart = (ulong*)method.DeclaringType.TypeHandle.Value.ToPointer();
+					ulong* address = classStart + index + skip;
+					return new IntPtr(address);
+				}
+				else
+				{
+					// Get the method table
+					uint* classStart = (uint*)method.DeclaringType.TypeHandle.Value.ToPointer();
+					uint* address = classStart + index + skip;
+					return new IntPtr(address);
+				}
+			}
+		}
+
+		private static IntPtr GetDynamicMethodRuntimeHandle(MethodBase method)
+		{
+			Console.WriteLine ("Gd1");
+			if (method is DynamicMethod)
+			{
+				FieldInfo fieldInfo = typeof(DynamicMethod).GetField("m_method", 
+					BindingFlags.NonPublic|BindingFlags.Instance);
+				return ((RuntimeMethodHandle)fieldInfo.GetValue(method)).Value;
+			}
+			Console.WriteLine ("Gd2");
+			return method.MethodHandle.Value;
+		}
+
+		public static void ReplaceMethod(IntPtr srcAdr, MethodBase dest)
+		{
+			Console.WriteLine ("rr1");
+			IntPtr destAdr = GetMethodAddress(dest);
+			Console.WriteLine ("rr2");
+			unsafe
+			{
+				if (IntPtr.Size == 8)
+				{
+					ulong* d = (ulong*)destAdr.ToPointer();
+					*d = *((ulong*)srcAdr.ToPointer());
+				}
+				else
+				{
+					uint* d = (uint*)destAdr.ToPointer();
+					*d = *((uint*)srcAdr.ToPointer());
+				}
+			}
+			Console.WriteLine ("rr3");
+		}
+		public static void ReplaceMethod(MethodBase source, MethodBase dest)
+		{
+//			if (!MethodSignaturesEqual(source, dest))
+//			{
+//				throw new ArgumentException("The method signatures are not the same.", 
+//					"source");
+//			}
+			Console.WriteLine ("Replace Method a");
+			ReplaceMethod(GetMethodAddress(source), dest);
+		}
+	}
+
+}
+
+
+
